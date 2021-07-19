d29552345939011f5a61993e0451ce0e7cec90c8
Alex <alex.hertel87@gmail.com>
2021-07-19 15:56:09 -0500

    edited table name in routes & forms

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request, render_template, flash
 from flask_login import login_required
-from app.models import User_Comments
+from app.models import User_Comment
 from app.forms import PostReview
 
 reviews_routes = Blueprint('reviews', __name__, url_prefix='/cats/reviews')
@@ -8,20 +8,20 @@ reviews_routes = Blueprint('reviews', __name__, url_prefix='/cats/reviews')
 
 @reviews_routes.route('/<int:id>', methods=['GET'])
 def reviews_index(id):
-    reviews = User_Comments.query.all()
+    reviews = User_Comment.query.all()
     return {review.id:review.to_dict for review in reviews}
 
 
-@reviews_routes.route('/reviews/new', methods=['GET', 'POST'])
+@reviews_routes.route('/new', methods=['GET', 'POST'])
 @login_required
 def post_review():
     form = PostReview()
     if form.validate_on_submit():
         flash(f'Your review has been posted!', 'success')
-        review = User_Comments(
+        review = User_Comment(
             user_id=session['user_id'],
             title=form.title.data['Title'],
             comment=request.form['Comment']
         )
         review.save()
-        return render_template('reviews/reviews.html', title='New Review', form=form)
+        return render_template('create_reviews.html', title='New Review', form=form)
