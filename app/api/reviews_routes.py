3408f9d7c04c2c175a464157dcbb05a81b859464
Alex <alex.hertel87@gmail.com>
2021-07-19 14:50:45 -0500

    added more to reviews routes

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,9 +1,27 @@
 from flask import Blueprint, jsonify, session, request
+from flask_login import login_required
 from app.models import User_Comments
+from app.forms import PostReview
 
 reviews_routes = Blueprint('reviews', __name__)
+
 
 @reviews_routes.route('/cat/reviews/<int:id>', methods=['GET'])
 def reviews(id):
     reviews = User_Comments.query.all()
-    return reviews.to_dict()
+    return {review.id:review.to_dict for review in reviews}
+
+
+@reviews_routes.route('/cat/reviews/<int:id>', methods=['POST'])
+@login_required
+def post_review():
+    form = PostReview()
+    if form.validate_on_submit():
+        review = User_Comments(
+            user_id=session['user_id'],
+            product_id=request.form['product_id'],
+            comment=request.form['comment']
+        )
+        review.save()
+        return jsonify({'message': 'Review posted successfully'})
+    return jsonify({'message': 'Review not posted'})
--- app/forms/reviews_form.py
+++ app/forms/reviews_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
-from app.models import User_Comments
+from app.models import User_Comments, db
 
 class PostReview(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
