378e2c96366c647ed98dc8efba60e866497cd135
Alex <alex.hertel87@gmail.com>
2021-07-19 17:51:48 -0500

    Think I completed Reviews route

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,27 +1,52 @@
-from flask import Blueprint, jsonify, session, request, render_template, flash
+from flask import Blueprint, jsonify, session, render_template, redirect
 from flask_login import login_required
-from app.models import User_Comment
+from app.models import User_Comment, db
 from app.forms import PostReview
 
-reviews_routes = Blueprint('reviews', __name__, url_prefix='/cats/reviews')
+reviews_routes = Blueprint('reviews', __name__)
 
 
-@reviews_routes.route('/<int:id>', methods=['GET'])
-def reviews_index(id):
-    reviews = User_Comment.query.all()
-    return {review.id:review.to_dict for review in reviews}
+@reviews_routes.route('/reviews/<int:id>')
+def reviews(id):
+    review = User_Comment.query.get_or_404(id)
+    return render_template('cats-page.html', review=review)
 
 
-@reviews_routes.route('/new', methods=['GET', 'POST'])
+@reviews_routes.route('/reviews/new', methods=['GET', 'POST'])
 @login_required
-def post_review():
+def new_review():
     form = PostReview()
     if form.validate_on_submit():
-        flash(f'Your review has been posted!', 'success')
-        review = User_Comment(
-            user_id=session['user_id'],
-            title=form.title.data['Title'],
-            comment=request.form['Comment']
-        )
-        review.save()
-        return render_template('create_reviews.html', title='New Review', form=form)
+        review = User_Comment(content=form.content.data, author=session['username'])
+        form.populate_obj(review)
+        db.session.add(review)
+        db.session.commit()
+        return redirect ('/cats/<int:id>')
+    return render_template('reviews_form.html', form=form)
+
+
+@reviews_routes.route('/reviews/<int:id>/update', methods=['GET', 'POST'])
+@login_required
+def update_review(id):
+    review = User_Comment.query.get_or_404(id)
+    if review.author != session['username'] and 'username' not in session:
+        return redirect('/login')
+    form = PostReview()
+    if form.validate_on_submit():
+        review.content = form.content.data
+        db.session.commit()
+        return redirect('/cats/<int:id>')
+    elif request.method == 'GET':
+        form.content.data = review.content
+    return render_template('reviews_form.html', form=form)
+
+
+@reviews_routes.route('/reviews/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_review(id):
+    review = User_Comment.query.get_or_404(id)
+    if review.author != session['username'] and 'username' not in session:
+        return redirect('/login')
+    db.session.delete(review)
+    db.session.commit()
+    return redirect('/cats/<int:id>')
