85ad918f665ad50b83b6e7b893603ad690fc5e35
Alex <alex.hertel87@gmail.com>
2021-07-19 15:06:07 -0500

    added create_reviews template

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -3,24 +3,24 @@ from flask_login import login_required
 from app.models import User_Comments
 from app.forms import PostReview
 
-reviews_routes = Blueprint('reviews', __name__)
+reviews_routes = Blueprint('reviews', __name__, url_prefix='/reviews')
 
 
-@reviews_routes.route('/cat/reviews/<int:id>', methods=['GET'])
+@reviews_routes.route('/', methods=['GET'])
 def reviews(id):
     reviews = User_Comments.query.all()
     return {review.id:review.to_dict for review in reviews}
 
 
-@reviews_routes.route('/cat/reviews/<int:id>', methods=['POST'])
+@reviews_routes.route('/cats/reviews/new', methods=['POST'])
 @login_required
 def post_review():
     form = PostReview()
     if form.validate_on_submit():
         review = User_Comments(
             user_id=session['user_id'],
-            product_id=request.form['product_id'],
-            comment=request.form['comment']
+            title=form.title.data['Title'],
+            comment=request.form['Comment']
         )
         review.save()
         return jsonify({'message': 'Review posted successfully'})
