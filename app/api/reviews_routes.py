7c00e9dcfbbb374ec228bc48b2439d5703e27120
Alex <alex.hertel87@gmail.com>
2021-07-19 14:09:37 -0500

    created reviews form

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, session, request
+from app.models import User_Comments
 
 reviews_routes = Blueprint('reviews', __name__)
 
-@reviews_routes.route('/reviews/<int:review_id>', methods=['GET'])
-
+@reviews_routes.route('/cat/reviews/<int:id>', methods=['GET'])
+def reviews(id):
+    reviews = User_Comments.query.all()
+    return reviews.to_dict()
---
+++ app/forms/reviews_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from app.models import User_Comments
+
+class PostReview(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Review', validators=[DataRequired()])
+    submit = SubmitField('Submit')
