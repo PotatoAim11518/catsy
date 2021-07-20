144d5c4b1869776927659426998cbe052826aef6
Alex <alex.hertel87@gmail.com>
2021-07-20 11:55:05 -0500

    created initial front-end files

---
+++ react-app/src/components/Scratches/Scratches_Form.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+
+
