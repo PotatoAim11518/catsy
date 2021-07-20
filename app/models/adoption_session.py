968b9031fc2db2e9d8bed92cbb002856d3317a77
Wilson Huang <52278252+PotatoAim11518@users.noreply.github.com>
2021-07-20 15:37:05 -0700

    Update adoption_session.py
    Fixed user_id typos

--- app/models/adoption_session.py
+++ app/models/adoption_session.py
@@ -14,7 +14,8 @@ class Adoption_Session(db.Model, UserMixin):
     def to_dict(self):
             return {
                 'id': self.id,
-                'user_id ': self.username,
+                'user_id': self.user_id,
                 'created_at': self.created_at,
                 'updated_at': self.updated_at
             }
+        
