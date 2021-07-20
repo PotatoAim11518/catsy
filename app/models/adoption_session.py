89e5bd582cf8e46dce83c8b33daa6a08d7d91b1f
Huy Lam <commanderh1995@gmail.com>
2021-07-20 17:33:36 -0500

    added to_dict method on all models

--- app/models/adoption_session.py
+++ app/models/adoption_session.py
@@ -9,3 +9,12 @@ class Adoption_Session(db.Model, UserMixin):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
     updated_at = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
+
+
+    def to_dict(self):
+            return {
+                'id': self.id,
+                'user_id ': self.username,
+                'created_at': self.created_at,
+                'updated_at': self.updated_at
+            }
