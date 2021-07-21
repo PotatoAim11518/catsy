ba8d21267f3e95d7178f61de2a30508a86bfaed5
Huy Lam <commanderh1995@gmail.com>
2021-07-21 07:09:00 -0500

    added homepage component

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,5 +1,6 @@
 import ExploreCats from './ExploreCats';
 import AboutMe from './AboutMe';
+import "./HomePage.css"
 
 const Homepage = () => {
   return (
@@ -7,6 +8,9 @@ const Homepage = () => {
       <div className="homepage-container">
         <ExploreCats />
         <AboutMe />
+        <div className="mailing-list">
+          <div>Hello World</div>
+        </div>
       </div>
     </>
   )
