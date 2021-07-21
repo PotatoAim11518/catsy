a7c773083fd637e3887f5ea17b0086cf3d1a4617
David <daveman@gmail.com>
2021-07-21 14:00:40 -0700

    🛠  WORKING ON: rendering cat components

---
+++ react-app/src/components/Cat.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { setCat } from "../store/cats";
+
+function catDetail() {
+
+}
+
+export default catDetail;
