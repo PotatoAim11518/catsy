9d8d5e80b11df108ab6951026d4c2dc00e050eba
Alex <alex.hertel87@gmail.com>
2021-07-22 17:37:14 -0500

    rudementary form complete

--- react-app/src/components/Comments/Comments_Form.js
+++ react-app/src/components/Comments/Comments_Form.js
@@ -7,7 +7,7 @@ import * as sessionActions from "../../store/session";
 
 export const Comments = ({ catScratch }) => {
    const dispatch = useDispatch();
-   const user = useSelector(state => state.session.user.id);
+   const user_id = useSelector(state => state.session.user.id);
    // const cat_id = useSelector(state => state.comments.cat_id);
    const [errors, setErrors] = useState(null);
    const [comment, setComment] = useState('');
@@ -25,8 +25,8 @@ export const Comments = ({ catScratch }) => {
 
       const catScratch = {
          comment,
-         user_id: 2,
-         cat_id: 4
+
+
       }
       dispatch(add_comment(catScratch));
    };
