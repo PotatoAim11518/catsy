d2a99d80db4eeaa78e2d5c1d270eb5e971d4e641
wilson <whuang3015@gmail.com>
2021-07-22 00:31:47 -0700

    styled cart list

--- react-app/src/components/Cart/CartItemList/index.js
+++ react-app/src/components/Cart/CartItemList/index.js
@@ -5,9 +5,18 @@ import CartItem from "./CartItem";
 export default function CartItemList({ cart_items }) {
   return (
     <>
+    <div>
       {cart_items && cart_items.map((item) => {
         return <CartItem key={item.id} item={item} />
       })}
+      {cart_items.length < 1 &&
+        <div style={{ backgroundImage: `url(/schrödingers_cat.jpg)` }}>
+          <img src="assets/schrödingers_cat.jpg" alt="unknown cat"/>
+          <h1>Your cart is empty.</h1>
+          <h2>(Probably)</h2>
+        </div>
+      }
+    </div>
     </>
   );
 }
