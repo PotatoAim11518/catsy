5d289aaa9e48d3a020b2e5fa04848eb4b92f0615
Huy Lam <commanderh1995@gmail.com>
2021-07-22 17:17:59 -0500

    working on displaying menu

--- app/api/categories_routes.py
+++ app/api/categories_routes.py
@@ -5,35 +5,31 @@ from app.models import Age, Gender, Size, Coat, Breed
 categories_routes = Blueprint('categories', __name__)
 
 @categories_routes.route('/ages')
-@login_required
 def ages():
+    print("Here")
     ages = Age.query.all()
     return {'ages': [age.to_dict() for age in ages]}
 
 
 @categories_routes.route('/genders')
-@login_required
 def genders():
     genders = Gender.query.all()
     return {'genders': [gender.to_dict() for gender in genders]}
 
 
 @categories_routes.route('/sizes')
-@login_required
 def sizes():
     sizes = Size.query.all()
     return {'sizes': [size.to_dict() for size in sizes]}
 
 
 @categories_routes.route('/coats')
-@login_required
 def coats():
     coats = Coat.query.all()
     return {'coats': [coat.to_dict() for coat in coats]}
 
 
 @categories_routes.route('/breeds')
-@login_required
 def breeds():
     breeds = Breed.query.all()
     return {'breeds': [breed.to_dict() for breed in breeds]}
---
+++ react-app/src/store/categories.js
@@ -0,0 +1,126 @@
+//ACTIONS
+const LOAD_AGES = 'categories/LOAD_AGES'
+const LOAD_SIZES = 'categories/LOAD_SIZES'
+const LOAD_COATS = 'categories/LOAD_COATS'
+const LOAD_BREEDS = 'categories/LOAD_BREEDS'
+const LOAD_GENDER = 'categories/LOAD_GENDER'
+
+
+//ACTION CREATORS
+const loadAges = list => ({
+  type: LOAD_AGES,
+  list
+})
+
+const loadSizes = list => ({
+  type: LOAD_SIZES,
+  list
+})
+
+const loadCoats = list => ({
+  type: LOAD_COATS,
+  list
+})
+
+const loadBreeds = list => ({
+  type: LOAD_BREEDS,
+  list
+})
+
+const loadGender = list => ({
+  type: LOAD_GENDER,
+  list
+})
+
+//THUNKS
+export const getAges = () => async dispatch => {
+  const response = await fetch(`/api/categories/ages`)
+  console.log(response)
+  if(response.ok) {
+    const list = await response.json();
+    dispatch(loadAges(list.ages));
+    console.log(list)
+  }
+}
+
+export const getSizes = () => async dispatch => {
+  const response = await fetch(`/api/categories/sizes`)
+
+  if(response.ok) {
+    const list = await response.json();
+    dispatch(loadSizes(list.sizes));
+    // console.log(list)
+  }
+}
+
+export const getCoats = () => async dispatch => {
+  const response = await fetch(`/api/categories/coats`)
+
+  if(response.ok) {
+    const list = await response.json();
+    dispatch(loadCoats(list.coats));
+    // console.log(list)
+  }
+}
+
+export const getBreeds = () => async dispatch => {
+  const response = await fetch(`/api/categories/breeds`)
+
+  if(response.ok) {
+    const list = await response.json();
+    dispatch(loadBreeds(list.breeds));
+    // console.log(list)
+  }
+}
+
+export const getGender = () => async dispatch => {
+  const response = await fetch(`/api/categories/genders`)
+
+  if(response.ok) {
+    const list = await response.json();
+    dispatch(loadGender(list.genders));
+    // console.log(list)
+  }
+}
+
+const intialState = {};
+const categoriesReducer = (state = intialState, action) => {
+  const newState = {...state}
+  switch(action.type) {
+    case LOAD_AGES: {
+      let agesArray = [];
+      console.log(">>>>><<<<<<<<",action)
+      action.list.forEach(age => agesArray.push(age.name))
+      newState["ages"] = agesArray;
+      return newState;
+    }
+    case LOAD_SIZES: {
+      let sizesArray = [];
+      action.list.forEach(size => sizesArray.push(size.name))
+      newState.sizes = sizesArray;
+      return newState;
+    }
+    case LOAD_COATS: {
+      let coatsArray = [];
+      action.list.forEach(coat => coatsArray.push(coat.name))
+      newState.coats = coatsArray;
+      return newState;
+    }
+    case LOAD_BREEDS: {
+      let breedsArray = [];
+      action.list.forEach(breed => breedsArray.push(breed.name))
+      newState.breeds = breedsArray;
+      return newState;
+    }
+    case LOAD_GENDER: {
+      let gendersArray = [];
+      action.list.forEach(gender => gendersArray.push(gender.name))
+      newState.genders = gendersArray;
+      return newState;
+    }
+    default:
+      return state;
+  }
+}
+
+export default categoriesReducer;
