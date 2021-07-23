6e3508d9ed015ea838db4f88a15c58e749b1ee12
wilson <whuang3015@gmail.com>
2021-07-23 11:36:06 -0700

    refactor cat patch route

--- app/api/cat_routes.py
+++ app/api/cat_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
+from flask_login.utils import login_required
 from app.models import Cat
-# from flask_login import login_required
+from flask_login import login_required
 
 cat_routes = Blueprint('cats', __name__)
 
@@ -11,8 +12,15 @@ def cats():
     return {'cats': [cat.to_dict() for cat in cats]}
 
 
-@cat_routes.route('/<int:id>', methods=["GET", "PATCH"])
-def cat(id, **payload):
+@cat_routes.route('/<int:id>')
+def cat(id):
+    cat = Cat.query.get(id)
+    return cat.to_dict()
+
+# CONTINUE HERE>>>>>>>.
+@cat_routes.route('/<int:id>', methods=["PATCH"])
+@login_required
+def catUpdate(id, **payload):
     cat = Cat.query.get(id)
     cat_dict = cat.to_dict()
     for attr, val in payload.items():
